430697d0f8aa9539d3dcaf6444c81eb924d70868
maurimnadal <maurim.nadal@gmail.com>
2024-05-14 16:23:46 -0300

    adicionei o arquivo operadores

---
+++ operadores/src/Operadores.java
@@ -0,0 +1,10 @@
+public class Operadores {
+    public static void main(String[] args) {
+
+        
+        String nomeUm = "Maurício";
+        String nomeDois = new String("Maurício");
+
+        System.out.println(nomeUm.equals(nomeDois));
+    }
+}
